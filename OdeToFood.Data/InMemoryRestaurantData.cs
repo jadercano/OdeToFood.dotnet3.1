941730b94df02aa63fc17454bc86080914a6e361
Jader Cano <jader.cano@contractors.accruent.com>
2020-08-21 19:02:52 -0500

    Finished Module 2 Drilling into Data. Added Bussiness, Domain, and Data Layer. Added Data Service.

---
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private readonly List<Restaurant> _restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>()
+                           {
+                               new Restaurant() { Id = 1, Name = "Jader's Pizza", Location = "Medellín, Colombia", CuisineType = CuisineType.Italian },
+                               new Restaurant() { Id = 1, Name = "Julieta's Indian Food", Location = "Medellín, Colombia", CuisineType = CuisineType.Indian },
+                               new Restaurant() { Id = 1, Name = "Natalia's Mexican", Location = "Medellín, Colombia", CuisineType = CuisineType.Mexican },
+                               new Restaurant() { Id = 1, Name = "Cano's Restaurant", Location = "Medellín, Colombia", CuisineType = CuisineType.Italian }
+                           };
+        }
+
+        #region Implementation of IRestaurantData
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants.OrderBy(r => r.Name);
+        }
+
+        #endregion
+    }
+}
--- OdeToFood.Web/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood.Web/Pages/Restaurants/List.cshtml.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
-namespace MyApp.Namespace
+namespace OdeToFood.Web.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IRestaurantData _restaurantData;
+
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+
         public void OnGet()
         {
+            Restaurants = _restaurantData.GetAll();
         }
     }
 }
