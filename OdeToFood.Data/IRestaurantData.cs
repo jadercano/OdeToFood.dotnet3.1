b263d2633340abe6416af3fd40b35308604270c7
Jader Cano <jader.cano@contractors.accruent.com>
2020-08-22 07:30:09 -0500

    Module 3 Working with Models and Model Binding. Added Page and Route to see a Details Page. Handling Bad Requests.

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -8,5 +8,7 @@ namespace OdeToFood.Data
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name = null);
+
+        Restaurant GetById(int id);
     }
 }
--- OdeToFood.Data/InMemoryRestaurantData.cs
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -13,9 +13,9 @@ namespace OdeToFood.Data
             _restaurants = new List<Restaurant>()
                            {
                                new Restaurant() { Id = 1, Name = "Jader's Pizza", Location = "Medellín, Colombia", CuisineType = CuisineType.Italian },
-                               new Restaurant() { Id = 1, Name = "Julieta's Indian Food", Location = "Medellín, Colombia", CuisineType = CuisineType.Indian },
-                               new Restaurant() { Id = 1, Name = "Natalia's Mexican", Location = "Medellín, Colombia", CuisineType = CuisineType.Mexican },
-                               new Restaurant() { Id = 1, Name = "Cano's Restaurant", Location = "Medellín, Colombia", CuisineType = CuisineType.Italian }
+                               new Restaurant() { Id = 2, Name = "Julieta's Indian Food", Location = "Medellín, Colombia", CuisineType = CuisineType.Indian },
+                               new Restaurant() { Id = 3, Name = "Natalia's Mexican", Location = "Medellín, Colombia", CuisineType = CuisineType.Mexican },
+                               new Restaurant() { Id = 4, Name = "Cano's Restaurant", Location = "Medellín, Colombia", CuisineType = CuisineType.Italian }
                            };
         }
 
@@ -26,6 +26,11 @@ namespace OdeToFood.Data
             return _restaurants
                        .Where(s => string.IsNullOrEmpty(name) || s.Name.Contains(name))
                        .OrderBy(r => r.Name);
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return _restaurants.Find(r => r.Id.Equals(id));
         }
 
         #endregion
